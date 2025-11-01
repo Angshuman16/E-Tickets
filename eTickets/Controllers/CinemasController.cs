00c8b6295819b1f10c1e95f3ee5b947e4f4c3379
Angshuman16 <pathakangshu@gmail.com>
2025-11-01 12:01:31 +0530

    Some Changes in the Integrating the Cinema using the BaseRepository

--- eTickets/Controllers/CinemasController.cs
+++ eTickets/Controllers/CinemasController.cs
@@ -1,5 +1,6 @@
 ﻿using eTickets.Data;
 using eTickets.Data.Services;
+using eTickets.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -29,5 +30,124 @@ namespace eTickets.Controllers
             var allCinemas = await _service.GetAllAsync();
             return View(allCinemas);
         }
+
+
+        public IActionResult Create()
+        {
+            return View();
+
+
+        }
+
+        [HttpPost]
+        public IActionResult Create([Bind("Logo,Name,Description")] Cinema cinema)  // Through this Bind We bind only the properties that we are gonna send from the Post Request.
+        {
+            //if (!ModelState.IsValid)
+            //{
+            //    return View(act);
+            //}
+
+            _service.AddAsync(cinema);
+            return RedirectToAction(nameof(Index));
+
+        }
+
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+
+            if (cinemaDetails == null)
+            {
+                return View("Empty");
+
+            }
+
+            return View(cinemaDetails);
+
+
+
+
+
+
+        }
+
+
+        public async Task<IActionResult> Edit(int id)
+        {
+
+
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null)
+            {
+                return View("Empty");
+            }
+            else return View(cinemaDetails);
+
+
+
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Logo,Name,Description")] Cinema cinema)  // Through this Bind We bind only the properties that we are gonna send from the Post Request.
+        {
+            if (!ModelState.IsValid)
+            {
+                foreach (var error in ModelState.Values.SelectMany(v => v.Errors))
+                {
+                    Console.WriteLine(error.ErrorMessage);
+                }
+            }
+
+            await _service.UpdateAsync(id, cinema);
+            return RedirectToAction(nameof(Index));
+
+        }
+
+
+
+        public async Task<IActionResult> Delete(int id)
+        {
+
+
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null)
+            {
+                return View("Empty");
+            }
+            else return View(cinemaDetails);
+
+
+
+        }
+
+
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)  // Through this Bind We bind only the properties that we are gonna send from the Post Request.
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+
+
+            if (cinemaDetails == null) return View("Empty");
+
+
+            await _service.DeleteAsync(id);
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
+
+
+
+
+
+
+
+
+
     }
 }
